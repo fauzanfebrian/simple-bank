37eafc8045beb4c42fc19dc2a4c5d912a5973576
fauzanfebrian <fauzanfebrian1725@gmail.com>
2023-09-15 15:16:02 +0700

    config load fixing

--- config/config.go
+++ config/config.go
@@ -1,18 +1,28 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/joho/godotenv"
 )
 
+const DB_DRIVER string = "postgres"
+
 var (
 	DB_SOURCE string
-	DB_DRIVER string = "postgres"
 )
 
 func init() {
-	godotenv.Load()
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+
+	if err := godotenv.Load(filepath.Join(basepath, "..", ".env")); err != nil {
+		fmt.Println("config error:", err)
+		return
+	}
 
 	DB_SOURCE = os.Getenv("DB_SOURCE")
 }
