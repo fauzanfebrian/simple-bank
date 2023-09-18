fd0136b05f8f8cb49b094badd5c9471a322bbffe
fauzanfebrian <fauzanfebrian1725@gmail.com>
2023-09-18 09:04:17 +0700

    update config load

--- config/config.go
+++ config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"fmt"
-
 	"github.com/joho/godotenv"
 )
 
@@ -12,15 +10,21 @@ var (
 	DBSource string
 )
 
+var mappingEnv = map[string]*string{
+	"DB_SOURCE": &DBSource,
+}
+
 // Before using config variables, you should run this function first to load it.
-func LoadConfig(envFullPath string) error {
-	envData, err := godotenv.Read(envFullPath)
+func LoadConfig(filenames ...string) error {
+	envData, err := godotenv.Read(filenames...)
 
 	if err != nil {
-		return fmt.Errorf("'%s' is not env file, please check again", envFullPath)
+		return err
 	}
 
-	DBSource = envData["DB_SOURCE"]
+	for key, variable := range mappingEnv {
+		*variable = envData[key]
+	}
 
 	return nil
 }
